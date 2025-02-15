59be4909f3b56b6e3e0d93ea4fcaaa667513ff63
IAmUser4574 <bauerlybriton6@gmail.com>
2025-02-14 20:28:09 -0600

    feat: flashlist working

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,59 @@
-import { Text, View } from "react-native";
+import { FlashList } from "@shopify/flash-list";
+import React from 'react';
+import {View, StyleSheet, Text, StatusBar} from 'react-native';
+import {SafeAreaView, SafeAreaProvider} from 'react-native-safe-area-context';
 
-export default function Index() {
+const DATA = [
+  {
+    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+    title: 'First Item',
+  },
+  {
+    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+    title: 'Second Item',
+  },
+  {
+    id: '58694a0f-3da1-471f-bd96-145571e29d72',
+    title: 'Third Item',
+  },
+];
+
+type ItemProps = {title: string};
+
+const Item = ({title}: ItemProps) => (
+  <View style={styles.item}>
+    <Text style={styles.title}>{title}</Text>
+  </View>
+);
+
+export default function Index(){
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+  <SafeAreaProvider>
+    <SafeAreaView style={styles.container}>
+      <FlashList
+        data={DATA}
+        renderItem={({item}) => <Item title={item.title} />}
+        keyExtractor={item => item.id}
+        estimatedItemSize={99}
+      />
+    </SafeAreaView>
+  </SafeAreaProvider>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight || 0,
+  },
+  item: {
+    backgroundColor: '#ddd',
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+    borderRadius: 15
+  },
+  title: {
+    fontSize: 32,
+  },
+});
